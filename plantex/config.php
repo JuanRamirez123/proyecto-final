4265287eb3a2acaa2f2a96ce839b71ee3e2c703b
JuanRamirez123 <109967766+JuanRamirez123@users.noreply.github.com>
2023-10-26 11:29:08 -0500

    Add files via upload

---
+++ plantex/config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'id20731958_sergio');
+define('DB_PASSWORD', 'Sergio#17');
+define('DB_NAME', 'id20731958_plantex');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($link === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+    
+}
---
+++ plantex/db.php
@@ -0,0 +1,5 @@
+<?php
+
+$conexion=mysqli_connect("localhost","id20731958_sergio","Sergio#17","id20731958_plantex");
+
+?>
